16ecb633a5b26b7cb79154956e41885ac7a6e7ec
Phat179 <thephat1709@gmail.com>
2024-06-25 18:34:48 +0700

    end

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
+// close navbar function
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
+
 
 if (bar) {
     bar.addEventListener('click', () => {
@@ -13,4 +15,28 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+
+
+
+
+// login popup
+const showPopupBtn = document.querySelector(".login-btn");
+const formPopup = document.querySelector(".form-popup");
+const hidePopupBtn = document.querySelector(".form-popup .close-btn"); 
+const loginSignupLink = document.querySelectorAll(".form-box .bottom-link a");
+
+showPopupBtn.addEventListener("click", () => {
+    document.body.classList.toggle("show-popup");
+});
+
+hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
+
+loginSignupLink.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        formPopup.classList[link.id === "signup-link" ? 'add' : 'remove']("show-signup");
+    });
+});
+
+
 
